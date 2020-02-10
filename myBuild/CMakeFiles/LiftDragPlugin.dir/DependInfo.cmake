586420eaf98d0091312f7deecc0ff92623b7ee5f
ysf_ultra <yusufmk93@gmail.com>
2020-02-10 09:34:09 +0300

    head olusturmak ne demek?

---
+++ myBuild/CMakeFiles/LiftDragPlugin.dir/DependInfo.cmake
@@ -0,0 +1,54 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nighthawk/PX4_Related/Firmware/Tools/sitl_gazebo/src/liftdrag_plugin/liftdrag_plugin.cpp" "/home/nighthawk/PX4_Related/Firmware/Tools/sitl_gazebo/myBuild/CMakeFiles/LiftDragPlugin.dir/src/liftdrag_plugin/liftdrag_plugin.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "LIBBULLET_VERSION=2.83"
+  "LIBBULLET_VERSION_GT_282"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/opencv"
+  "/usr/include/gazebo-9"
+  "/usr/include/bullet"
+  "/usr/include/simbody"
+  "/usr/include/sdformat-6.2"
+  "/usr/include/ignition/math4"
+  "/usr/include/OGRE"
+  "/usr/include/OGRE/Terrain"
+  "/usr/include/OGRE/Paging"
+  "/usr/include/ignition/transport4"
+  "/usr/include/ignition/msgs1"
+  "/usr/include/ignition/common1"
+  "/usr/include/ignition/fuel_tools1"
+  "../include"
+  "."
+  "/usr/include/eigen3"
+  "/usr/include/eigen3/eigen3"
+  "/usr/include/gazebo-9/gazebo/msgs"
+  "/home/nighthawk/PX4_Related/Firmware/mavlink/include"
+  "/usr/include/Paging"
+  "../external/OpticalFlow/include"
+  "/usr/include/uuid"
+  "/usr/include/x86_64-linux-gnu"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/nighthawk/PX4_Related/Firmware/Tools/sitl_gazebo/myBuild/CMakeFiles/mav_msgs.dir/DependInfo.cmake"
+  "/home/nighthawk/PX4_Related/Firmware/Tools/sitl_gazebo/myBuild/CMakeFiles/nav_msgs.dir/DependInfo.cmake"
+  "/home/nighthawk/PX4_Related/Firmware/Tools/sitl_gazebo/myBuild/CMakeFiles/std_msgs.dir/DependInfo.cmake"
+  "/home/nighthawk/PX4_Related/Firmware/Tools/sitl_gazebo/myBuild/CMakeFiles/sensor_msgs.dir/DependInfo.cmake"
+  "/home/nighthawk/PX4_Related/Firmware/Tools/sitl_gazebo/myBuild/CMakeFiles/physics_msgs.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
